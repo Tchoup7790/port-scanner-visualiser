d2bbc114b9d29e506144e5dd58f1b0ff2846f3b8
Baptiste <113362872+Tchoup7790@users.noreply.github.com>
2026-04-15 23:38:57 +0200

    Gestion des protocoles de base

--- main.go
+++ main.go
@@ -1,23 +1,66 @@
 package main
 
 import (
+	"crypto/tls"
 	"flag"
 	"fmt"
 	"net"
 	"sort"
+	"strings"
 	"sync"
 	"time"
 )
 
-func scanPort(host string, port int) bool {
+func scanPort(host string, port int) (bool, string) {
+	dialer := &net.Dialer{
+		Timeout: 1 * time.Second,
+	}
+
 	address := net.JoinHostPort(host, fmt.Sprintf("%d", port))
-	conn, err := net.DialTimeout("tcp", address, 1*time.Second)
+
+	var conn net.Conn
+	var err error
+
+	conn, err = tls.DialWithDialer(dialer, "tcp", address, &tls.Config{
+		InsecureSkipVerify: true,
+	})
 	if err != nil {
-		return false
+		conn, err = net.DialTimeout("tcp", address, 1*time.Second)
+	}
+	if err != nil {
+		return false, ""
+	}
+
+	_ = conn.SetReadDeadline(time.Now().Add(2 * time.Second))
+
+	buf := make([]byte, 1024)
+	n, _ := conn.Read(buf)
+
+	protocole := string(buf[:n])
+
+	if protocole == "" {
+		_, _ = conn.Write([]byte("HEAD / HTTP/1.0\r\nHost: " + host + "\r\n\r\n"))
+
+		_ = conn.SetReadDeadline(time.Now().Add(2 * time.Second))
+
+		buf := make([]byte, 1024)
+		n, _ := conn.Read(buf)
+
+		protocole = strings.Split(string(buf[:n]), " ")[0]
+	}
+
+	if protocole == "" {
+		protocole = "undefined"
+	}
+
+	_, isTLS := conn.(*tls.Conn)
+	if isTLS && protocole != "undefined" {
+		protocole = "HTTPS"
 	}
 
 	_ = conn.Close()
-	return true
+
+	return true, string(protocole)
 }
 
 func main() {
@@ -40,9 +83,10 @@ func main() {
 			defer wg.Done()
 			defer func() { <-sem }()
 
-			if scanPort(host, p) {
+			isOpen, protocole := scanPort(host, p)
+			if isOpen {
 				mu.Lock()
-				openPortsMap[p] = "OPEN"
+				openPortsMap[p] = protocole
 				mu.Unlock()
 			}
 		}(port)
@@ -54,8 +98,10 @@ func main() {
 	for k := range openPortsMap {
 		keys = append(keys, k)
 	}
+
 	sort.Ints(keys)
 
+	fmt.Println("PORT", "PROTOCOLE")
 	for _, k := range keys {
 		fmt.Println(k, openPortsMap[k])
 	}
